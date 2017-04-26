15cb668391cbcab89056ea5637c81c56cae910a2
Unknown <spablomendoza@mail.csuchico.edu>
2017-04-25 23:10:01 -0700

    Added some variables and fixed the function

--- gallery.js
+++ gallery.js
@@ -1,6 +1,14 @@
-
-
+var chico = ["WREC","Bidwell Park","Downtown","Chico Museum","Science Museum"];
+var index = 1000;
 function shuffleGallery()
 {
-
+  var arrayLength = chico.length;
+  var newIndex = 0;
+  do {
+    newIndex = Math.floor(Math.random() * arrayLength);
+  }
+  while (currentIndex == newIndex);
+  currentIndex = newIndex;
+  console.log(chico[newIndex]);
+}
 }
